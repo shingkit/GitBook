354f8562403ada35e4bb543cee40d9ca59c9397a
sxlcssj <sxlcssj@163.com>
2015-12-16 17:19:27 +0800

    rxbinding添加searchview
    结合rxjava 过滤三秒内的文字输入

--- app/src/main/java/com/shingkit/gitbook/rxAndroid/rxbinding/RxBindingActivity.java
+++ app/src/main/java/com/shingkit/gitbook/rxAndroid/rxbinding/RxBindingActivity.java
@@ -2,18 +2,23 @@ package com.shingkit.gitbook.rxAndroid.rxbinding;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.jakewharton.rxbinding.support.v7.widget.RxSearchView;
 import com.jakewharton.rxbinding.view.RxView;
 import com.jakewharton.rxbinding.widget.RxTextView;
 import com.shingkit.gitbook.R;
 
+import java.util.concurrent.TimeUnit;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import rx.functions.Action1;
 
 /**
  * Created by LENOVO on 2015/12/16.
@@ -26,12 +31,15 @@ public class RxBindingActivity extends AppCompatActivity {
     @Bind(R.id.tv_rxbinding)
     TextView tvBinding;
 
+    @Bind(R.id.sv_rxbinding)
+    SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_binding);
         ButterKnife.bind(this);
-        
+
         RxView.clicks(btnRxBinding1).subscribe((ev) -> {
             tvBinding.setText("点击上面的按钮，我变啦！！！");
         });
@@ -40,7 +48,10 @@ public class RxBindingActivity extends AppCompatActivity {
             Toast.makeText(RxBindingActivity.this, "文字变了，我也变！", Toast.LENGTH_SHORT).show();
         });
 
-
+        RxSearchView.queryTextChanges(searchView).
+                throttleFirst(3000, TimeUnit.MILLISECONDS).subscribe((text) -> {
+            Toast.makeText(RxBindingActivity.this, "查询文字" + text, Toast.LENGTH_SHORT).show();
+        });
     }
 
 
